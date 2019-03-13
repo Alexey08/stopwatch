677b7d7ac4bc644e765383fb0d80330802d55267
Alexey08 <bauer080@gmail.com>
2019-03-13 20:53:13 +0500

    Initial commit

---
+++ app/src/main/java/com/example/alexey/stopwatch/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.alexey.stopwatch;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int one;
+    private boolean running;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button resetButton = (Button)findViewById(R.id.button2);
+        resetButton.setEnabled(false);
+
+        runTimer();
+    }
+
+    public void onClickStart(View view) {
+
+        Button startStopButton = (Button)findViewById(R.id.button);
+        Button resetButton = (Button)findViewById(R.id.button2);
+
+                if (running) {
+                    running = false;
+                    startStopButton.setText("Start");
+                    resetButton.setEnabled(false);
+                } else  {
+                    running = true;
+                    startStopButton.setText("Stop");
+                    resetButton.setEnabled(true);
+                }
+
+    }
+
+
+    public void onClickReset(View view) {
+
+        running = false;
+        one = 0;
+        Button startStopButton = (Button)findViewById(R.id.button);
+        startStopButton.setText("Start");
+
+    }
+
+    private void runTimer() {
+        final TextView TextView = (TextView)findViewById(R.id.time_View);
+        final Handler Handler = new Handler();
+        Handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours= one/36000;
+                int minuts = one/600%60;
+                int secononds = one/10%60;
+                int milliseconds = one%10;
+
+                String time = String.format("%02d:%02d:%02d.%d", hours, minuts, secononds, milliseconds);
+                TextView.setText(time);
+                if (running) {
+                    one++;
+                }
+                Handler.postDelayed(this,100);
+            }
+        });
+    }
+}
